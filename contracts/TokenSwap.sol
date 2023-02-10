1787b229599c6509af98d6cbb6f128434c256562
maroo <maroo@gmail.com>
2023-02-10 11:12:11 +0630

    added MyToken sol file close #8

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity >=0.4.21 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/IERC20.sol";
 
