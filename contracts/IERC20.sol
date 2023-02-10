13594fb76bf76a67c02ccf6e91dd5e568b3c7a30
maroo <maroo@gmail.com>
2023-02-10 09:30:09 +0630

    add IERC20 sole file close #3

---
+++ contracts/IERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.21 <0.9.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (bool);
+
+    function transfer(address recipient, uint amount) external view returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approval(address spender, uint amount) external returns (bool);
+
+    function transferFrom(address spender, address recipient, uint amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
